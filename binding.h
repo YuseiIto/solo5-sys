758cb4ad25913b6d6212fea2513efaf328b5e761
Yusei Ito <me@yuseiito.com>
2022-07-18 08:31:42 +0000

    Move binding.h

---
+++ binding.h
@@ -0,0 +1,3 @@
+#include "solo5/include/solo5.h"
+#include "solo5/bindings/bindings.h"
+#include "solo5/bindings/lib.c"
--- src/binding.h
+++
@@ -1,3 +0,0 @@
-#include "../solo5/bindings/lib.c"
-#include "../solo5/include/solo5.h"
-
